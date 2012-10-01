5b3b67b63a81ca9d6fab03d8117336b4ada14372
kaleeswaran <kaleeswaran.subbaiya@photoninfotech.net>
2012-10-01 23:22:12 +0530

    RedQueen Corporations Database Driven Development Started

---
+++ DBDrivenFramework/Devlopment/db-driven-webapp/src/main/java/corp/redqueen/WelcomeUserAction.java
@@ -0,0 +1,20 @@
+package corp.redqueen;
+
+public class WelcomeUserAction {
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	// all struts logic here
+	public String execute() {
+
+		return "SUCCESS";
+
+	}
+}
---
+++ DBDrivenFramework/Devlopment/db-driven-webapp/src/main/webapp/welcome_user.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<html>
+<head></head>
+<body>
+	<h1>RedQueen Corporation welcomes you !!!</h1>
+ 
+	<h4>
+		Hello
+		<s:property value="username" />
+	</h4>
+ 
+</body>
+</html>
